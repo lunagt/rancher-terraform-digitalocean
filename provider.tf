74cc81374d3fb43ec092ebb3e686d1e9cb24a0d4
Gepser Hoil <geharold@gmail.com>
2017-02-13 10:27:53 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,6 +3,8 @@ variable "pub_key" {}
 variable "pvt_key" {}
 variable "ssh_fingerprint" {}
 variable "host_name" {}
+variable "domain" {}
+variable "domain_name" {}
 
 provider "digitalocean" {
    token = "${var.do_token}"
