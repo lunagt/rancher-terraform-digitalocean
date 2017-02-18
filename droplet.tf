efe8b33088b3367340684138d32aa6e3eb2f920b
Gepser Hoil <geharold@gmail.com>
2017-02-18 02:31:43 -0600

    Adding iptables configuration, installation and exposing a volume for the mysql on rancher

--- droplet.tf
+++ droplet.tf
@@ -1,6 +1,6 @@
 
 resource "digitalocean_droplet" "host" {
-    image = "coreos-stable"
+    image = "docker-16-04"
     name = "${var.host_name}"
     region = "sfo1"
     size = "1gb"
@@ -11,7 +11,7 @@ resource "digitalocean_droplet" "host" {
 
     connection {
       type = "ssh"
-      user = "core"
+      user = "root"
       private_key = "${file(var.pvt_key)}"
       timeout = "2m"
     }
@@ -19,9 +19,20 @@ resource "digitalocean_droplet" "host" {
     provisioner "remote-exec" {
       inline = [
         "sudo iptables -I INPUT 1 -i eth0 -p tcp --dport 8080 -j ACCEPT",
-        "docker run -d --restart=unless-stopped -p 8080:8080 rancher/server:stable"
+        "sudo /sbin/iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080",
+        "sudo DEBIAN_FRONTEND=noninteractive apt-get -y install iptables-persistent",
+        "sudo netfilter-persistent save",
+        "sudo netfilter-persistent reload",
+        "docker run -d -v /var/lib/mysql:/var/lib/mysql --restart=unless-stopped -p 8080:8080 rancher/server:stable"
         ]
       }
+}
+
+resource "digitalocean_record" "host" {
+  domain = "${var.domain}"
+  type = "A"
+  name = "${var.domain_name}"
+  value = "${digitalocean_droplet.host.ipv4_address}"
 }
 
 output "output-host" {
