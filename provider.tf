b2b0fac705d1bab4cbaf243b8fe8580a168091e8
Gepser Hoil <geharold@gmail.com>
2017-03-12 22:45:03 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,13 +3,13 @@ variable "pub_key" {}
 variable "pvt_key" {}
 variable "ssh_fingerprint" {}
 variable "domain" {}
-variable "domain_name" {}
-variable "host_name" { default = "rancher-server"}
-variable "image" { default = "docker-16-04"}
-variable "region" { default = "sfo1"}
-variable "size" { default = "1gb"}
-variable "private_networking" { default = "false"}
-variable "user" { default = "rancher"}
+variable "domain_name" { default = "rancher." }
+variable "host_name" { default = "rancher-server" }
+variable "image" { default = "docker-16-04" }
+variable "region" { default = "sfo1" }
+variable "size" { default = "1gb" }
+variable "private_networking" { default = "false" }
+variable "user" { default = "rancher" }
 
 provider "digitalocean" {
    token = "${var.do_token}"
